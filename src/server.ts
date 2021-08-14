4c87812fb51110a6d396b72c46fca0d7d1bb4902
dcamy <diegocamyy@gmail.com>
2021-08-14 15:40:56 -0300

    First commit

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+
+const PORT = process.env.PORT || 1234;
+
+app.get("/", (req, res) => {
+  res.send("HELLO");
+});
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
